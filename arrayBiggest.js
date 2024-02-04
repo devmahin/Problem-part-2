2152707aca91bc349a4668ccc7639107e437e562
devmahin <mahinhowlader123@gmail.com>
2024-02-04 20:26:16 +0600

    array biggest

---
+++ arrayBiggest.js
@@ -0,0 +1,15 @@
+let array1 = [11,23,25,65,64,65,76,86]
+
+function avarageLearge(arr){
+    let arrayBig = arr[0];
+    for(let array of arr){
+        // console.log(array)
+        if(array > arrayBig){
+            arrayBig = array;
+        }
+    }
+    return arrayBig
+
+}
+let biggerst = avarageLearge(array1);
+console.log(biggerst)
--- maxValue.js
+++ maxValue.js
@@ -1,7 +1,38 @@
-let mahin = 56;
-let adib = 34;
-if(mahin > adib){
-    console.log("Mahin stoberi kha")
-}else{
-    console.log("adib tuii kha")
-}
+// 1111111111111111111
+
+// let mahin = 56;
+// let adib = 64;
+// if(mahin > adib){
+//     console.log("Mahin stoberi kha")
+// }else{
+//     console.log("adib tuii kha")
+// }
+
+
+
+
+
+// 222222222222222222222222222
+// let mahin = 56;
+// let adib = 64;
+// let antor = 54;
+
+// function maxval(num1, num2, num3){
+//     if(num1 > num2 && num1 > num3){
+//         return "Antor is bigger"
+//     }else if(num2 > num1 && num2 > num3 ){
+//         console.log("adib is bigger")
+//     }else{
+//         console.log("mahin is bigger")
+//     }
+
+// }
+// maxval(mahin, adib, antor)
+
+
+
+
+// 3333333333333333333
+// let array = 
+let max = Math.max(12,34,56,74,244,64)
+console.log(max)
