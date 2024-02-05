8171c27a9a2b1d19c62e343fca00f2ce0ea6b79f
devmahin <mahinhowlader123@gmail.com>
2024-02-05 23:42:00 +0600

    task 4 solved

--- task.js
+++ task.js
@@ -31,3 +31,38 @@ for (let i = 1; i < strings.length; i++) {
 }
 
 console.log("Friend with the smallest name:", smallestName);
+
+
+
+
+// Task-3:
+// Your task is to calculate the total budget required to buy electronics:
+// Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
+
+
+function totalBudget (){
+    let mobile = 20000;
+    let laptop = 35000;
+    let tablet = 15000;
+
+    let totals = mobile + laptop + tablet;
+    return totals;
+}
+let total = totalBudget()
+console.log(total)
+
+
+// Task-4:
+// You are given an array of phone objects, each containing information about the model, brand, and price. Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
+
+// Input
+
+
+    const phones = [
+        { model: "PhoneA", brand: "Iphone", price: 95000 },
+        { model: "PhoneB", brand: "Samsung", price: 40000 },
+        { model: "PhoneC", brand: "Oppo", price: 26000 },
+        { model: "PhoneD", brand: "Nokia", price: 35000 },
+        { model: "PhoneE", brand: "Iphone", price: 105000 },
+        { model: "PhoneF", brand: "HTC", price: 48000 },
+    ];
