629924a1c34abe38b34f52c369121fd1e77c283d
devmahin <mahinhowlader123@gmail.com>
2024-02-06 14:12:55 +0600

    task all completed

--- task.js
+++ task.js
@@ -21,7 +21,7 @@
 
 // Task -2:
 // Find the friend with the smallest name.
-const strings = ['rahim', 'robin', 'rafi', 'ron', 'rashed', "df", "mah","a"];
+const strings = ['rahim', 'robin', 'rafi', 'ron', 'rashed', "df", "mah", "a"];
 let smallestName = strings[0];
 for (let i = 1; i < strings.length; i++) {
     // console.log(strings[i].length)
@@ -30,7 +30,7 @@ for (let i = 1; i < strings.length; i++) {
     }
 }
 
-console.log("Friend with the smallest name:", smallestName);
+// console.log("Friend with the smallest name:", smallestName);
 
 
 
@@ -40,16 +40,18 @@ console.log("Friend with the smallest name:", smallestName);
 // Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
 
 
-function totalBudget (){
+function totalBudget(mobileQuantity, laptopQuantity, tabletQuantity) {
     let mobile = 20000;
     let laptop = 35000;
     let tablet = 15000;
 
-    let totals = mobile + laptop + tablet;
-    return totals;
+    let tortal = mobile * mobileQuantity + laptop * laptopQuantity + tabletQuantity * tablet;
+
+    // let totals = mobile + laptop + tablet;
+    return tortal;
 }
-let total = totalBudget()
-console.log(total)
+let total = totalBudget(1, 2, 20)
+// console.log(total)
 
 
 // Task-4:
@@ -58,23 +60,75 @@ console.log(total)
 // Input
 
 
-    const phones = [
-        { model: "PhoneA", brand: "Iphone", price: 95000 },
-        { model: "PhoneB", brand: "Samsung", price: 40000 },
-        { model: "PhoneC", brand: "Oppo", price: 26000 },
-        { model: "PhoneD", brand: "Nokia", price: 35000 },
-        { model: "PhoneE", brand: "Iphone", price: 105000 },
-        { model: "PhoneF", brand: "HTC", price: 48000 },
-    ];
+const phones = [
+    { model: "PhoneA", brand: "Iphone", price: 95000 },
+    { model: "PhoneB", brand: "Samsung", price: 40000 },
+    { model: "PhoneC", brand: "Oppo", price: 26000 },
+    { model: "PhoneD", brand: "Nokia", price: 35000 },
+    { model: "PhoneE", brand: "Iphone", price: 105000 },
+    { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+
+
+
+
+
+
+
+
+function findAveragePhonePrice(phones) {
+    // console.log(phones.price)
+    if (phones.length === 0 || typeof phones !== "object") {
+        return "Plese object submit"
+    }
+
+    let total = 0;
+    let len = phones.length;
+
+    for (let pho of phones) {
+
+        total += pho.price / len;
+    }
+
+    return total;
+
+}
+
+
+
+
+// console.log(
+//     findAveragePhonePrice(phones)
+
+// )
+
 
 
 
 //     Task -5: (Hard)
 // For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
 
- const employees = [
-            { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
-            { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
-            { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
-            { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
-        ];
+const employees = [
+    { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+    { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+    { name: "shikot", experience: 10, starting: 30000, increment: 1000 },
+    { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+];
+
+function employeeSalary(employ) {
+    let total = 0;
+    let totalInc = 0;
+
+    for (let i = 0; i < employ.length; i++) {
+        let incrementYear = employ[i].increment * employ[i].experience
+        // console.log(incrementYear)
+        totalInc += incrementYear;
+        total += employ[i].starting;
+    }
+    return totalInc + total;
+}
+console.log(
+employeeSalary(employees)
+)
+
